84f241a5a501ef68b8446c4d4f4f1ffa75859e2f
Jordan Deren <jordanderen1@gmail.com>
2021-03-25 18:52:19 +0100

    fixed

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+    :address => 'smtp.sendgrib.net',
+    :port => '587',
+    :authentication => :plain,
+    :user_name => Rails.application.credentials.sendgrid[:usernmame],
+    :password => Rails.application.credentials.sendgrid[:api_key],
+    :domain => 'heroku.com',
+    :enable_starttls_auto => true
+}
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,10 +11,13 @@ Rails.application.configure do
   # and those relying on copy on write to perform better.
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => 'https://rails-photo-app-jderen.herokuapp.com/', :protocol => 'https'}
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
+
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
