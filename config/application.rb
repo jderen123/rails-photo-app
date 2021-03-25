b70bc0cf2edd86c6cb3f0db356a2641877a706f8
Jordan Deren <jordanderen1@gmail.com>
2021-03-25 18:59:07 +0100

    done

--- config/application.rb
+++ config/application.rb
@@ -11,8 +11,9 @@ Bundler.require(*Rails.groups)
 module RailsPhotoApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.1
     config.assets.initialize_on_precompile = false
+    config.load_defaults 6.1
+    
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
