199e1b23ea73591ba67eae63ae3b537cca76b40d
Jordan Deren <jordanderen1@gmail.com>
2021-03-27 01:58:16 +0100

    add production

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,5 +1,4 @@
 require("@rails/ujs").start()
-require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 //= require jquery
@@ -7,4 +6,4 @@ require("channels")
 //= require twitter/bootstrap
 //= require_tree .
 import "bootstrap"
-import "./credit_card_form"
+import "./credit_card_form.js"
--- app/models/payment.rb
+++ app/models/payment.rb
@@ -11,9 +11,9 @@ class Payment < ApplicationRecord
     end 
 
     def process_payment
-        customer = Stripe::Customer.create email: email, card: token
+        customer = Stripe::Customer.create(email: email, card: token)
 
-        Stripe::Charge.create customer: customer.id, amount: 1000, description: "Premium", currency: "USD"
+        Stripe::Charge.create(customer: customer.id, amount: 1000, description: "Premium", currency: "USD")\
 
     end 
 end
